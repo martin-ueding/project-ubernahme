7d0fdeb42dd33436d4ad13302d168b140353cefb
Martin Ueding <dev@martin-ueding.de>
2010-12-19 12:50:59 +0100

    added seperator to description

--- sources/projectubernahme/Localizer.java
+++ sources/projectubernahme/Localizer.java
@@ -1,7 +1,7 @@
 package projectubernahme;
 
-/** translates stings
-  There have to be translated strings in sting_lang.properties files, in order for this to work. At this point, it just returns the string that is queried since the program is changing rapidly and any translation would not do any good right now. */
+/** translates stings -
+ There have to be translated strings in sting_lang.properties files, in order for this to work. At this point, it just returns the string that is queried since the program is changing rapidly and any translation would not do any good right now. */
 public class Localizer {
 	public static String get (String query) {
 		return query;
