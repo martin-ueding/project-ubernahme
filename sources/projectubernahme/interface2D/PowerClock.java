30adfdc28ac59f44b95695f8d6d9d99cd8367f52
Martin Ueding <dev@martin-ueding.de>
2011-02-04 18:40:22 +0100

    powerclock without file saving

--- sources/projectubernahme/interface2D/PowerClock.java
+++ sources/projectubernahme/interface2D/PowerClock.java
@@ -1,9 +1,6 @@
 package projectubernahme.interface2D;
 
 import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Calendar;
 
 public class PowerClock {
@@ -11,24 +8,11 @@ public class PowerClock {
 	boolean first = true;
 	long clockBegin;
 
-	public PowerClock() {
-		try {
-			os = new BufferedOutputStream(new FileOutputStream("power.csv"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
 	public void start() {
 		clockBegin = Calendar.getInstance().getTimeInMillis();
 	}
 
 	public void end(String task) {
-		try {
-			os.write((String.valueOf(Calendar.getInstance().getTimeInMillis()-clockBegin)+",").getBytes());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		if (first) {
 			System.out.print(task+",");
 		}
@@ -38,11 +22,6 @@ public class PowerClock {
 	}
 
 	public void done() {
-		try {
-			os.write("\n".getBytes());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 		System.out.println();
 		first = false;
 	}
